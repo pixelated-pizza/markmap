d0774eeaf5d377c96163c267b9c0f8cd444ab546
Ralph Rivera <millstrading.ralph.r@gmail.com>
2025-10-06 08:49:05 +1100

    Fix campaigns layout

--- resources/js/components/Campaigns.vue
+++ resources/js/components/Campaigns.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col w-full h-full bg-gray-900 p-10">
+  <div class="flex flex-col w-full h-full bg-gray-900 p-10" v-cloak>
     <div class="flex items-center justify-between gap-2 p-4 mb-4 
             bg-gray-800/80 backdrop-blur-md shadow-xl border border-gray-700 rounded-xl">
       <h2 class="text-md font-bold  flex-1 text-white 
