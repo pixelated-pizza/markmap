9ae164ad8e3e9f7fafc37a8ae0000c2134298ef7
Ralph Rivera <millstrading.ralph.r@gmail.com>
2026-02-26 10:50:14 +1100

    Configure Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             SmaAccountSeeder::class,
             WebsiteCampaignTypeSeeder::class,
             EditorAccountSeeder::class,
+            AddAdhocPromoSeeder::class,
         ]);
     }
 }
