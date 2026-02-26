756d291804910e2892d55215dfcd7a5223da9a9d
Ralph Rivera <millstrading.ralph.r@gmail.com>
2026-02-26 12:42:32 +1100

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
-    base: import.meta.env.VITE_APP_URL + "/",
+    base: VITE_APP_URL.endsWith("/") ? VITE_APP_URL : VITE_APP_URL + "/",
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
