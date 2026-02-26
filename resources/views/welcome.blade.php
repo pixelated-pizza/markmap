6a983aebeb5a97b42546aa47c203f051ff421107
Ralph Rivera <millstrading.ralph.r@gmail.com>
2026-02-26 12:00:43 +1100

    Remove error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MarketMap</title>
     
-    @vite('resources/css/app.css', 'resources/js/api/api.js')
+    @vite('resources/css/app.css')
 </head>
 
 <body class="">
