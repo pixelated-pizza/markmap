fd0286627aae44bb79ef4de438df82441acb1a1f
Ralph Rivera <millstrading.ralph.r@gmail.com>
2026-03-04 14:53:23 +1100

    WSD api fix

--- resources/js/api/wsd_api.js
+++ resources/js/api/wsd_api.js
@@ -4,26 +4,26 @@ const api = axios.create({
 });
 
 export async function fetchWSD() {
-  const { data } = await api.get("api/website_sale_details");
+  const { data } = await api.get("website_sale_details");
   return data;
 }
 
 export async function createWSD(wsd) {
-  const { data } = await api.post("api/website_sale_details", wsd);
+  const { data } = await api.post("website_sale_details", wsd);
   return data;
 }
 
 export async function updateWSD(id, updates) {
-  const { data } = await api.put(`api/website_sale_details/${id}`, updates);
+  const { data } = await api.put(`website_sale_details/${id}`, updates);
   return data;
 }
 
 export async function deleteWSD(id) {
-  await api.delete(`api/website_sale_details/${id}`);
+  await api.delete(`website_sale_details/${id}`);
   return true;
 }
 
 export async function fetchBlankWSD(wc_id) {
-  const { data } = await api.get(`api/website_sale_details/blank/${wc_id}`);
+  const { data } = await api.get(`website_sale_details/blank/${wc_id}`);
   return data.data; 
 }
